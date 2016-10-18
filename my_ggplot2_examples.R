8c36bdb61bb17aba95631a0d50f4bf3637bde476
rrogerr <rgcbll@gmail.com>
2016-10-18 05:31:48 -0500

    lost the count

--- my_ggplot2_examples.R
+++ my_ggplot2_examples.R
@@ -1,5 +1,6 @@
 library(ggplot2)
 str(mpg)
+data("mtcars")
 
 qplot(displ, hwy, data = mpg)
 
@@ -43,3 +44,21 @@ print(i)
 j <- i + geom_smooth(method = "lm", se = FALSE, size = 4, linetype = 3, color = "red")
 print(j)
 j + theme_bw(base_family = "Times")
+
+# Axis limits
+testdata <- data.frame(x = 1:100, y = rnorm(100))
+testdata[50, 2] <- 100 #outlier
+plot(testdata$x, testdata$y, type ="l", ylim = c(-3,3))
+
+A <- ggplot(testdata, aes(x = x, y = y))
+B <- A + geom_line()
+
+B + ylim(-3,3) #excludes the outlier from the dataset
+B + coord_cartesian(ylim = c(-3,3)) #includes the point but doesn't plot it
+
+# categorize a continuous variable
+cutpoints <- quantile(mtcars$wt, seq(0, 1, length = 4), na.rm = TRUE)
+mtcars$new_fact <- cut(mtcars$wt, cutpoints)
+levels(mtcars$new_fact)
+ggplot(mtcars, aes(mpg, disp)) + geom_point() + facet_grid(.~new_fact)
+ggplot(mtcars, aes(mpg, disp)) + geom_point() + facet_grid(cyl~new_fact)
